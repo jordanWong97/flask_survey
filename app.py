e40251ec2a15f39dc9e95c95ab1d75bbaffcb910
Jordan Wong <pjwong97@gmail.com>
2022-07-28 11:43:16 -0700

    compelte step 1 and 2

--- app.py
+++ app.py
@@ -7,3 +7,17 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def index():
+    """renders page to show user title of survey"""
+    return render_template('survey_start.html',
+                        survey_title = survey.title,
+                        survey_instructions = survey.instructions)
+
+@app.post('/begin')
+def begin():
+    """when user clicks start button, should redirect to questions/0 route"""
+    return redirect('/questions/0')
