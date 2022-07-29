0b24b0f27c82599e812ff8089fd3d174cc0738ff
Raytene Han <raytenehan@gmail.com>
2022-07-28 18:03:04 -0700

    further study

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, current_app, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from pkg_resources import require
-from surveys import satisfaction_survey as survey
+from surveys import surveys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
@@ -11,16 +11,25 @@ debug = DebugToolbarExtension(app)
 
 
 
+# @app.get('/')
+# def index():
+#     """renders page to show user title of survey"""
+#     return render_template('survey_start.html',
+#                         survey = survey)
+
 @app.get('/')
 def index():
     """renders page to show user title of survey"""
-    return render_template('survey_start.html',
-                        survey = survey)
+
+    return render_template('survey_select.html',
+                        surveys = surveys)
+                        # surveys = surveys) # list of keys ["satisfaction", "personality"]
 
 @app.post('/begin')
 def begin():
     """when user clicks start button, should redirect to questions/0 route"""
     session['responses'] = []
+    session['survey'] = request.form.get('survey-dropdown')
     return redirect('/questions/0')
 
 @app.get('/questions/<int:question_num>')
@@ -29,13 +38,15 @@ def questions(question_num):
 
     current_question_num = len(session['responses']) #min 29-32
 
-    if current_question_num == len(survey.questions):
+    print(session['survey'])
+    breakpoint()
+    if current_question_num == len(session['survey'].questions):
         return redirect('/thanks')
     elif question_num != current_question_num:
         flash('Stop that!')
         return redirect(f'/questions/{current_question_num}')
     else:
-        question = survey.questions[question_num]
+        question = session['survey'].questions[question_num]
         return render_template('question.html',
                                 question = question,
                                 )
@@ -49,7 +60,7 @@ def answer():
     responses.append(request.form['answer'])
     session['responses'] = responses
     question_num = len(responses)
-    if question_num < len(survey.questions):
+    if question_num < len(session['survey'].questions):
         return redirect(f'/questions/{question_num}')
     else:
         return redirect('/thanks')
